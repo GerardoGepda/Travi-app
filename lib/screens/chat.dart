2dd64661094dac555dd170d58fb13254b8b5ff9d
roger <nestor.roger@outlook.com>
2020-08-22 12:58:17 -0600

    hola

---
+++ lib/screens/chat.dart
@@ -0,0 +1,267 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MyHeader(),
+      //bottomNavigationBar: CreateMessage(),
+      body: ChatBody(),
+    );
+  }
+}
+
+class MyHeader extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 35.0, left: 17.0, right: 17.0, bottom: 20.0),
+      color: const Color(0xFF0d80eb),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+                IconButton(
+                  padding: EdgeInsets.all(0),
+                  icon: Icon(Icons.close, color: Colors.white, size: 30,),
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                ),
+                IconButton(
+                  padding: EdgeInsets.all(0),
+                  icon: Icon(Icons.phone, color: Colors.white, size: 30,),
+                  onPressed: (){
+                  },
+                ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "TRAVI",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 23.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Icon(
+                Icons.more_vert,
+                color: Colors.white,
+                size: 30.0,
+              ),
+            ],
+          ),
+        ],
+      ), 
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(120);
+}
+
+class ChatBody extends StatelessWidget {
+
+  final String texto = "Buena día. A través de este medio podra comunicarse con nuestro equipo en caso de un incidente o problema dentro de la unidad o sobre nuestra plataforma.";
+  final String texto2 = "Buena día. Quisiera reportar una conducción temeraria por parte del motorista de la ruta 41-F, rumbo a Soyapango en la cual me encuentro en estos momentos.";
+  final String texto3 = "Agradecemos la información, se realizará un reporte sobre la unidad y sobre su conductor, y se enviara a la empresa correspondiente.";
+  final String texto4 = "Muchas gracias por su atención y por su trabajo.";
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(bottom: 50.0),
+          color: Colors.grey[50],
+          child: ListView(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 20, bottom: 30),
+                alignment: Alignment.center,
+                child: Text(
+                  "Martes 4 de Agosto de 2020",
+                  style: TextStyle(
+                    color: const Color(0xFF0d80eb),
+                  ),
+                ),
+              ),
+              _receivedMessage(texto, "9:01 AM"),
+              _myMesagge(texto2, "9:02 AM"),
+              _receivedMessage(texto3, "9:03 AM"),
+              _myMesagge(texto4, "9:04 AM"),
+            ],
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: CreateMessage(),
+        ),
+      ],
+    );
+  }
+
+
+  Container _myMesagge(String message, String messageHour){
+    return Container(
+      margin: EdgeInsets.only(bottom: 35.0),
+      alignment: Alignment.centerLeft,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            width: 50.0,
+            height: 50.0,
+            margin: EdgeInsets.only(left:20.0, right: 10.0),
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/images/users/user1.jpg'),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            constraints: BoxConstraints(
+              minWidth: 70.0,
+              maxWidth: 250.0,
+            ),
+            decoration: BoxDecoration(
+              color: const Color(0xFF0d80eb),
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 15.0,
+                  spreadRadius: -5,
+                  offset: Offset(1.0, 4.0),
+                ),
+              ],
+            ),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  message,
+                  maxLines: 6,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Text(
+                    messageHour,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+  Container _receivedMessage(String message, String messageHour){
+    return Container(
+      margin: EdgeInsets.only(bottom: 35.0),
+      alignment: Alignment.centerRight,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(10),
+            constraints: BoxConstraints(
+              minWidth: 75.0,
+              maxWidth: 250.0,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 15.0,
+                  spreadRadius: -5,
+                  offset: Offset(1.0, 4.0),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  message,
+                  maxLines: 6,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.justify,
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Text(messageHour),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 50.0,
+            height: 50.0,
+            margin: EdgeInsets.only(left: 20.0, right: 10.0),
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/images/users/travi.jpg'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CreateMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 55.0,
+      margin: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+      padding: EdgeInsets.only(left: 15, right: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black,
+            blurRadius: 15.0,
+            spreadRadius: -5,
+            offset: Offset(1.0, 5.0),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            width: 310.0,
+            child: TextField(
+              textAlignVertical: TextAlignVertical.center,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Escribe un mensaje",
+              ),
+            ),
+          ),
+          Icon(
+            Icons.mic,
+            color: Colors.black,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/profile.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MyHeader(),
+      body: profileData,
+    );
+  }
+
+
+  final Widget profileData = Center(
+    child: Container(
+      padding: EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 10),
+      margin: EdgeInsets.only(left: 15, right: 15),
+      height: 200,
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Icon(
+                Icons.equalizer,
+                color: const Color(0xFF0d80eb),
+              ),
+              Text(
+                "Estadisticas",
+                style: TextStyle(
+                  color: const Color(0xFF0d80eb),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _circleOfData(Colors.green, "32", "Viajes", "realizados"),
+              _circleOfData(Colors.blue, "10", "Comentarios", "realizados"),
+              _circleOfData(Colors.yellow[700], "50", "Estrellas", "otorgadas"),
+              _circleOfData(Colors.red[400], "223", "Travi", "puntos"),
+            ],
+          ),
+          Container(
+            alignment: Alignment.bottomRight,
+            child: Text(
+              "DETALLES",
+              style: TextStyle(
+                color: const Color(0xFF0d80eb),
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+
+  static Column _circleOfData(Color color, var data, String txt1, String txt2){
+    return Column(
+      children: <Widget>[
+        Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color,
+          ),
+          child: Center(
+            child: Text(
+              data.toString(),
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        Text(
+          txt1,
+        ),
+        Text(
+          txt2,
+        ),
+      ],
+    );
+  }
+
+
+}
+
+class MyHeader extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 35, left: 10, right: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15), bottomRight: Radius.circular(15)),
+        color: const Color(0xFF0d80eb),
+      ),
+      child: Column(
+        //mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.close, size: 30, color: Colors.white,),
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    "Perfil",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Icon(
+                  Icons.person,
+                  color: Colors.white,
+                  size: 30,
+                ),
+                ],
+              ),
+            ],
+          ),
+          Stack(
+            children: <Widget>[
+              Positioned(
+                child: Container(
+                  margin: EdgeInsets.only(top: 10),
+                  width: 110,
+                  height: 110,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/users/user1.jpg'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Text(
+            "@Juan123",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(215);
+}
